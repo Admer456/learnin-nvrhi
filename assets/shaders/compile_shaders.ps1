74f5ca4a3bfbdd61ef17b4e403610f7efa7619d0
Admer <49600504+Admer456@users.noreply.github.com>
2022-08-27 14:31:48 +0200

    Shader folder structure to support all 3 APIs

--- assets/shaders/compile_shaders.ps1
+++ assets/shaders/compile_shaders.ps1
@@ -1,2 +1,8 @@
 
-.\nvrhi-scomp.exe -c "D:/dxc/bin/dxc.exe" -i shaders.cfg -o . -P SPIRV -D SPIRV
+## I installed DXC into D:/dxc
+## Yours might be in a different location
+$DXC = "D:/dxc/bin/x64/dxc.exe"
+
+.\nvrhi-scomp.exe -c $DXC -i shaders.cfg -o "vk/" -P SPIRV -D SPIRV
+.\nvrhi-scomp.exe -c $DXC -i shaders.cfg -o "dx12/" -P DXIL -D DXIL
+.\nvrhi-scomp.exe -c $DXC -i shaders.cfg -o "dx11/" -P DXBC -D DXBC
