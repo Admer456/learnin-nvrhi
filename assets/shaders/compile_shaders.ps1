9ee8fc35371a399b49ad985d77b5cb3aa54e15f2
Admer <49600504+Admer456@users.noreply.github.com>
2022-08-31 23:50:19 +0200

    Use FXC for DX11 shaders, and force recompilation of them

--- assets/shaders/compile_shaders.ps1
+++ assets/shaders/compile_shaders.ps1
@@ -2,7 +2,8 @@
 ## I installed DXC into D:/dxc
 ## Yours might be in a different location
 $DXC = "D:/dxc/bin/x64/dxc.exe"
+$FXC = "D:/DXSDK_June2010/Utilities/bin/x64/fxc.exe"
 
-.\nvrhi-scomp.exe -c $DXC -i shaders.cfg -o "vk/" -P SPIRV -D SPIRV
-.\nvrhi-scomp.exe -c $DXC -i shaders.cfg -o "dx12/" -P DXIL -D DXIL
-.\nvrhi-scomp.exe -c $DXC -i shaders.cfg -o "dx11/" -P DXBC -D DXBC
+.\nvrhi-scomp.exe -c $DXC -i shaders.cfg -o "vk/" -P SPIRV -D SPIRV -f
+.\nvrhi-scomp.exe -c $DXC -i shaders.cfg -o "dx12/" -P DXIL -D DXIL -f
+.\nvrhi-scomp.exe -c $FXC -i shaders.cfg -o "dx11/" -P DXBC -D DXBC -f
